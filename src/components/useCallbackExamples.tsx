2a38143066bc9beb9f2c84fa5a6f08fd868e2aab
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-05-29 12:48:46 -0700

    minor tweaks/cleanup around useCallback

--- src/components/useCallbackExamples.tsx
+++ src/components/useCallbackExamples.tsx
@@ -1,6 +1,5 @@
-import { useCallback, useState } from 'react'
-import Search from './search';
-import { Box, List, ListItem, Typography } from '@mui/material';
+import {memo, useCallback, useState} from 'react'
+import {Box, List, ListItem, TextField, Typography} from '@mui/material';
 
 const fruitList = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew'];
 
@@ -9,7 +8,7 @@ interface CallbackExamplesListProps {}
 function CallbackExamples_List({}: CallbackExamplesListProps) {
   const [fruit, setFruit] = useState(fruitList);
 
-  // By default, functions in react are different on every render. This is because functions are objects in JavaScript,
+  // By default, functions in React are different on every render. This is because functions are objects in JavaScript,
   // and objects are compared by reference. When React re-renders a component, it does not reuse function instances
   // unless they are memoized by using useCallback.
   const handleSearch = useCallback((text: string): void => {
@@ -39,3 +38,19 @@ function CallbackExamples_List({}: CallbackExamplesListProps) {
 }
 
 export default CallbackExamples_List
+
+/************************************************************************************************************/
+// normally this search component would be in a separate file
+
+interface SearchProps { onSearch: (query: string) => void; }
+
+// Memoizing the Search component to prevent unnecessary re-renders. This only looks at the props passed
+// to it, not the state of the parent component. Even so, since the 'onSearch' prop is a function, and by
+// default in React functions are reinstantiated on each re-render, we also need to wrap the 'onSearch'
+// function in a useCallback hook so that React will re-use the existing instance (so long as all the listed
+// dependencies didn't change.)
+const Search = memo(function({ onSearch }: SearchProps) {
+    console.log("Search component rendered");
+
+    return <TextField type="text" variant="outlined" placeholder="Search..." onChange={(e) => onSearch(e.target.value)} />;
+});
